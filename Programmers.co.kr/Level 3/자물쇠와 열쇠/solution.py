09a421f99227244f5dc695872b555b79a75f874a
woodykang <59731599+woodykang@users.noreply.github.com>
2021-04-18 14:58:40 +0900

    Update solution.py
    Corrected error. Right answer for the problem.

--- Programmers.co.kr/Level 3/자물쇠와 열쇠/solution.py
+++ Programmers.co.kr/Level 3/자물쇠와 열쇠/solution.py
@@ -1,27 +1,30 @@
 def solution(key, lock):
-    N = len(key[0])         # key  is N x N array
-    M = len(lock[0])        # lock is M x M array
+    M = len(key[0])         # key  is M x M array
+    N = len(lock[0])        # lock is N x N array
 
-    for rot_time in range(3):       # rotate key 3 times
+    for rot_time in range(4):       # rotate key 3 times
 
         ## Shifting key
-        for x in range(1, 2*N - 1):         # shift in x axis
-            for y in range(1, 2*N - 1):     # shift in y axis
+        for x in range(1, N + M + 1):         # shift in x axis
+            for y in range(1, N + M + 1):     # shift in y axis
 
+                ## XOR operation to elements of rotated & shifted key and lock
                 result = True
-                for i in range(M):
-                    for j in range(M):
-                        ## XOR to elements of key and lock
-                        if (i in range( max(0, x - N), min(x, N) )) & (j in range( max(0, y - N), min(y, N) )):
-                            result &= key[N - x + i][N - y + j] ^ lock[i][j]
+                for i in range(N):
+                    for j in range(N):
+                        ## Case 1: rotated & shifted key and lock overlap
+                        if (i in range( max(0, x - M), min(x, N) )) & (j in range( max(0, y - M), min(y, N) )):
+                            result &= key[M - x + i][M - y + j] ^ lock[i][j]
 
+                        ## Case 2: key and lock does not overlap
                         else:
                             result &= lock[i][j]
                 
+                ## Return True if result == True
                 if result:
                     return True
         
         ## Rotate  key
-        key = [[key[N - 1 - j][i] for j in range(N)] for i in range(N)]
+        key = [[key[M - 1 - j][i] for j in range(M)] for i in range(M)]
 
     return False
